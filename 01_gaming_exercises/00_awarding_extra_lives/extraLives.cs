8d938707b630911db1b21ce728d804267256f21a
Chickenbuckit <111383048+Chickenbuckit@users.noreply.github.com>
2024-09-04 14:40:05 -0400

    v0.0

---
+++ 01_gaming_exercises/00_awarding_extra_lives/extraLives.cs
@@ -0,0 +1,26 @@
+//extra lives, Casey Boyce, v0.0
+class extraLives {
+  static void Main() {
+    int livesRemaining = 3
+    Console.WriteLine("You have " + livesRemaining + "lives left\n");
+    Console.WriteLine("You gained points! But how many:\n");
+    int points = Console.ToInt32(Console.ReadLine());
+    Console.WriteLine("You gained points! But how many: " + points);
+    if (points >= 10000)
+    {
+        livesRemaining --;
+    }
+    else if (points <= 10001 && points >= 10000)
+    {
+        livesRemaining ++;
+    }
+    else if (points <= 10000)
+    {
+        livesRemaining --;
+    }
+    else
+    {
+        Console.WriteLine("3Rr0R");
+    }
+  }
+}
