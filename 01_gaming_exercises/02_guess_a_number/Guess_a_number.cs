3072b023f1a4b698ec1ef60df5a1aca93245edc3
Chickenbuckit <111383048+Chickenbuckit@users.noreply.github.com>
2024-09-06 14:54:37 -0400

    v0.1

--- 01_gaming_exercises/02_guess_a_number/Guess_a_number.cs
+++ 01_gaming_exercises/02_guess_a_number/Guess_a_number.cs
@@ -7,37 +7,56 @@ class GuessANumber {
     int numGuess = 0;
     int maxGuess = 7; // 7 guesses
     //start of random number generator                                      WHILE LOOP
-    int hiddenNumber;
-    if mode == "easy";
+    int secrateNumber;
+    Console.WriteLine('Do You need "easy" mode or do you want "hard" mode\nYou have 7 guesses ether way');
+    mode = Console.ToLower(readline());
+    if (mode == "easy")
     {
-        int hiddenNumber = rnd.Next(999); //generate from 0 to 998
-        hiddenNumber++;//999*
+        int secrateNumber = rnd.Next(999); //generate from 0 to 998
+        secrateNumber++;//999*
         int x = 1;
         int y = 999;
     }
-    else;
+    else
     {
-        int hiddenNumber = rnd.Next(25,76); //generate from 25 to 75
-        x = 25
-        y = 75
+        int secrateNumber = rnd.Next(25,76); //generate from 25 to 75
+        x = 25;
+        y = 75;
     }
-    while (numGeuss > maxGuess);
+    while (numGuess < maxGuess)
     {
-        Console.WriteLine("Pick a number from any where from" x "and" y );
-        int guess = Consolt.ToInt32(readline());
-        Consolt.Writeline("You guessed:"guess"\n");
-        numGuess++;
                                                                             //start of player input code
-
-                                                                            //start of comparing number code
-
-                                                                            //start of matching, less, or more result code
-
+        Console.WriteLine("Pick a number from any where from "+x+"  and  "+y+);
+        int guess = Consolt.ToInt32(readline());
+        Consolt.Writeline("You guessed: "guess"\n");
                                                                             //++ to numGuess
-
-                                                                            //numGeuss == maxGuess?
-
-                                                                            //true print "YOU LOSE" and exit
+        numGuess++;
+                                                                            //start of comparing number code
+                                                                            //start of matching, less, or more result code
+        if (guess < secrateNumber)
+        {
+            Console.WriteLine("To Low T-T\n");
+        }
+        else if (guess > secrateNumber)
+        {
+            Console.WriteLine("To High T-T\n");
+        }
+                                                                            //numGuess == maxGuess?
+        else
+        {
+            Console.WriteLine("Correct ;3\n");
+            break; // keword to stop a loop
+        }
     }
+                                                                                //if true print "YOU LOSE" and exit
+    if (numGuess == maxGuess)
+    {
+        Console.WriteLine("                     YOU LOSE .u.")
+    }
+    else
+    {
+        Console.WriteLine("3Rr0R")
+    }
+    exit;
   }
 }
