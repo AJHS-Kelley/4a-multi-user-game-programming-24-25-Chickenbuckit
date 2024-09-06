c78725a24a3edc06efa7e9b2b91cb9c7013fd9e4
Chickenbuckit <111383048+Chickenbuckit@users.noreply.github.com>
2024-09-06 13:58:09 -0400

    v0.0

---
+++ 01_gaming_exercises/02_guess_a_number/Guess_a_number.cs
@@ -0,0 +1,21 @@
+//Guess A Number, Casey Boyce, v0.0
+using System;
+class GuessANumber {
+  static void Main() {
+    int numGuess = 0;
+    int maxGuess = 7;
+    //start of random number generator                                      WHILE LOOP
+    int hiddenNumber;
+                                                                            //start of player input code
+
+                                                                            //start of comparing number code
+
+                                                                            //start of matching, less, or more result code
+
+                                                                            //++ to numGuess
+
+                                                                            //numGeuss == maxGuess?
+
+                                                                            //true print "YOU LOSE" and exit
+  }
+}
