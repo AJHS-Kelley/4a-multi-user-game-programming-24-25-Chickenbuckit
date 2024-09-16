adf802372864181f1501f979fcf45e78cfea7d1e
Chickenbuckit <111383048+Chickenbuckit@users.noreply.github.com>
2024-09-16 14:04:07 -0400

    v0.3

--- 01_gaming_exercises/04_rock_paper_scissors/RrockPaperSissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/RrockPaperSissors.cs
@@ -1,4 +1,4 @@
-//Rock papers sissors, Casey Boyce, v0.2
+//Rock papers sissors, Casey Boyce, v0.3
 using System;
 class rockPaperSissors {
     static void Main()
@@ -26,6 +26,28 @@ class rockPaperSissors {
             //Player choice
             Console.WriteLine("Choose Rock (1), Paper (2), or Scissors (3): ");
             playerChoice = int.Parse(Console.ReadLine());
+            //CPU choice
+            Random rand = new Random();
+            cpuChoice = rand.Next(1, 4); // 1, 2, or 3
+
+            //Determine the winner
+            string result;
+            if (playerChoice == cpuChoice)
+            {
+                result = "It's a tie!";
+            }
+            else if ((playerChoice == 1 && cpuChoice == 3) ||
+                     (playerChoice == 2 && cpuChoice == 1) ||
+                     (playerChoice == 3 && cpuChoice == 2))
+            {
+                result = $"{playerName} wins this round!";
+                playerScore++;
+            }
+            else
+            {
+                result = "CPU wins this round!";
+                cpuScore++;
+            }
         }
     }
 }
