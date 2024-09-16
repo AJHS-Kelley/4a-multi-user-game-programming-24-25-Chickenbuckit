860be306e3efbd21cfb767b9ddaedc900484f816
Chickenbuckit <111383048+Chickenbuckit@users.noreply.github.com>
2024-09-16 13:49:22 -0400

    v0.1

--- 01_gaming_exercises/04_rock_paper_scissors/RrockPaperSissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/RrockPaperSissors.cs
@@ -1,7 +1,16 @@
-//Rock papers sissors, Casey Boyce, v0.0
+//Rock papers sissors, Casey Boyce, v0.1
 using System;
-class rockPaper {
-  static void Main() {
-    
-  }
+class rockPaperSissors {
+    static void Main()
+    {
+        // Declare variables
+        string playerName = "Test Player";
+        int playerScore = 0;
+        int cpuScore = 0;
+        int playerChoice;
+        int cpuChoice;
+        // Get player's name
+        Console.WriteLine("Enter your name: ");
+        playerName = Console.ReadLine();
+    }
 }
