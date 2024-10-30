634a3495723e0560c5e12cd5ad8072be9d16c974
Chickenbuckit <s11896446@students.duvalschools.org>
2024-10-30 12:39:28 -0400

    v0.0

---
+++ 00_computer_science_exercises/04_collections/04_collectionsPlease.cs
@@ -0,0 +1,23 @@
+//Collections example, Casey Boyce, v0.0
+using System;
+using System.Linq;
+using System.Collections
+
+class Template {
+  static void Main() {
+    /*
+in C# an collections is known as an ARRAY.
+--Each item in the array is known as an element
+-- Number of elements in the array cannot be changed
+-- Contence of each elements can be changed
+-- Arrays are ordered, meaning each items has a fixed position.
+-- Position of the element is called an INDEX
+-- First element index is 0
+    */
+    // Declaring  and defining an array
+    string[] breakfastFoods = {"Cereal", "Bacon", "Pancakes", "Milk"};
+    int[] testScore = {15, 64, 80, 90, 100};
+    double[] GPA = {0.31D, 2.37D, 1.75D, 4.00D}
+    
+   } //NEVER DELETE, also only three spaces needed
+} //NEVER DELETE, also always touching left side
