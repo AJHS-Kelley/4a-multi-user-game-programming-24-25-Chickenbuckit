bde9bf6fa2c13186e6ec3871c6626dd9c115bb4c
Chickenbuckit <s11896446@students.duvalschools.org>
2024-10-30 12:52:14 -0400

    v0.1

--- 00_computer_science_exercises/04_collections/04_collectionsPlease.cs
+++ 00_computer_science_exercises/04_collections/04_collectionsPlease.cs
@@ -1,13 +1,12 @@
-//Collections example, Casey Boyce, v0.0
+//Collections example, Casey Boyce, v0.1
 using System;
 using System.Linq;
-using System.Collections
-
-class Template {
+using System.Collections;
+class Collections {
   static void Main() {
     /*
 in C# an collections is known as an ARRAY.
---Each item in the array is known as an element
+-- Each item in the array is known as an element
 -- Number of elements in the array cannot be changed
 -- Contence of each elements can be changed
 -- Arrays are ordered, meaning each items has a fixed position.
@@ -17,7 +16,10 @@ in C# an collections is known as an ARRAY.
     // Declaring  and defining an array
     string[] breakfastFoods = {"Cereal", "Bacon", "Pancakes", "Milk"};
     int[] testScore = {15, 64, 80, 90, 100};
-    double[] GPA = {0.31D, 2.37D, 1.75D, 4.00D}
-    
+    double[] GPA = {0.31D, 2.37D, 1.75D, 4.00D};
+    // Print array contents -- All elements on a single line
+    Console.WriteLine("The elements of each array are:\n");
+    Console.WriteLine("Breakfast foods:\n" + String.Join("\n", breakfastFoods));
+    Console.WriteLine();// Print an empty line at end
    } //NEVER DELETE, also only three spaces needed
 } //NEVER DELETE, also always touching left side
