5f2ec773c478a84835e5c0c81bda3147d393674c
Chickenbuckit <s11896446@students.duvalschools.org>
2024-11-18 13:43:03 -0500

    v0.0

---
+++ 00_computer_science_exercises/04_collections/04_moreArrays.cs
@@ -0,0 +1,14 @@
+// Casey Boyce, Even More Arrays, YAY, v0.0
+using System;
+using System.Collections;
+using System.Linq;
+class moreArrays {
+  static void Main() {
+    string[] colors = {"Garnet", "Pearl", "Amathist", "Rose pink", "Steaven"};
+    // Looping arrays -- the old way
+    for (int i = 0; i < collors.length[i]; i++)
+    {
+        Console.WriteLine(colors[i])
+    }
+   } //NEVER DELETE, also only three spaces needed
+} //NEVER DELETE, also always touching left side
